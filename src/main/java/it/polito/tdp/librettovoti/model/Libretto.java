3e712e64043681cb4c382de05b2270621e646634
Vito Vattiata <vito.vattiata@olivetti.com>
2021-12-21 12:53:01 +0100

    Soluzione punto 2 con metodo List e metodo Oggetto.

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -24,5 +24,25 @@ public class Libretto {
         return result;
     }
 
+    public List<Voto> listaVotiUguali(int punteggio) {
+        List<Voto> risultato = new ArrayList<>();
+        for(Voto v: this.voti){
+            if(v.getVoto() == punteggio){
+                risultato.add(v);
+            }
+        }
+        return risultato;
+    }
+
+    public Libretto votiUguali(int punteggio){
+        Libretto risultato = new Libretto();
+        for (Voto v: this.voti){
+            if(v.getVoto()==punteggio){
+                risultato.add(v);
+            }
+        }
+        return risultato;
+    }
+
 
 }
